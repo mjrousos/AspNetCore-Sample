1076e16005e2ea20a0beeca9945641f5ff81d550
Mike Rousos <MikeRou@Microsoft.com>
2017-03-21 23:06:14 -0400

    Add simple middleware example

--- src/CustomersAPI/Startup.cs
+++ src/CustomersAPI/Startup.cs
@@ -10,6 +10,10 @@ using System.Globalization;
 using Microsoft.AspNetCore.Localization;
 using System.Resources;
 using System.Reflection;
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace CustomerAPI
 {
@@ -65,6 +69,31 @@ namespace CustomerAPI
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            // Middleware: Trivial middleware registration to demonstrate how
+            //             to add a step to the app's HTTP request processing pipeline.
+            // Middleware: Note that middleware will execute in the pipeline in the order it is registered.
+            //             Since this trivial middleware is meant to time how long the entire processing
+            //             of request takes, it is included very early in the middleware pipeline.
+            var timingLogger = loggerFactory.CreateLogger("Timing Middleware");
+            app.Use(async (HttpContext context, Func<Task> next) =>
+            {
+                // Middleware: Logic to run regarding the request prior to invoking more middleware.
+                //             This logic should be followed by either a call to next() (to invoke the next
+                //             piece of middleware) or to context.Response.WriteAsync/SendFileAsync
+                //             without calling next() (which will end the middleware pipeline and begin
+                //             travelling back up the middleware stack.
+                var timer = new Stopwatch();
+                timer.Start();
+
+                // Middleware: Calling the next delegate will invoke the next piece of middleware
+                await next();
+
+                // Middleware: Code after 'next' will usually run after another piece of middleware
+                //             has written a response, so context.Response should not be written to here.
+                timer.Stop();
+                timingLogger.LogInformation($"Request to {context.Request.Method}:{context.Request.Path} processed in {timer.ElapsedMilliseconds} ms");
+            });
+
             /* Add Global/Localization support for more information see 
                https://docs.asp.net/en/latest/fundamentals/localization.html */
             var supportedCultures = new[]
@@ -73,7 +102,7 @@ namespace CustomerAPI
                     new CultureInfo("es-MX"),
                     new CultureInfo("fr-FR"),
               };
-
+            
             app.UseRequestLocalization(new RequestLocalizationOptions
             {
                 DefaultRequestCulture = new RequestCulture("en-US"),
@@ -82,6 +111,8 @@ namespace CustomerAPI
                 // UI strings that we have localized.
                 SupportedUICultures = supportedCultures
             });
+
+            app.UseStaticFiles();
 
             app.UseMvc();
 
