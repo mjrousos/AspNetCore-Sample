021ab1ac1fd48f400f5431bcec25e1eb4f50bdb5
Jon Jung (WEB) <jonjung@microsoft.com>
2017-05-11 23:25:32 +0000

    Merged PR 388: Add missing stylecop.json, update .gitignore and fix up warnings for rules
    Add missing stylecop.json, update .gitignore and fix up warnings for rules

--- test/CustomersDemoTestHelpers/TestLogger.cs
+++ test/CustomersDemoTestHelpers/TestLogger.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// Licensed under the MIT license. See LICENSE file in the samples root for full license information.
+
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 
