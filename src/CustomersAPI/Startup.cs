51a3b3369fb21ec9d07b4f79f0fdc4109d2286f8
Charles Lowell <chlowe@microsoft.com>
2017-07-27 13:44:34 -0700

    stop using obsolete UseInMemoryDatabase overload

--- src/CustomersAPI/Startup.cs
+++ src/CustomersAPI/Startup.cs
@@ -68,7 +68,7 @@ namespace CustomerAPI
 
             // Add Entity Framework Customers data provider
             services.AddDbContext<EFCustomersDataProvider>(options =>
-                options.UseInMemoryDatabase());
+                options.UseInMemoryDatabase("CustomerDataDb"));
 
             // Dependency Injection: This could be added directly to services (via AddScoped),
             // but in this sample we're adding it to the Autofac container in order to
